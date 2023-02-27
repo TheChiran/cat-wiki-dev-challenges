1d85eec0ba1a26e3e5d1645f1eb4f1ecab4caef9
TheChiran <tonmoychiran449@gmail.com>
2023-02-27 20:56:40 +0600

    [Task-> 1. Made main layout]

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -1,26 +1,27 @@
-import {Routes, Route, Navigate} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Footer from '../../components/Footer/Index';
+import Header from '../../components/Header/Index';
+import Home from '../../views/Home/Index';
+
 import './App.css'
-import Sidebar from '../../components/Sidebar/Index';
-import Buttons from '../../views/Buttons/Index';
 
 function App() {
   return (
     <div className="App">
       <div className="main-layout">
-        <div className="nav-section">
-          <Sidebar/>
+        <div className="header-section">
+          <Header />
         </div>
         <div className="content-section">
           <div className="main-content">
-          <Routes>
-            <Route path="/" element={<Navigate to={"/buttons"}/>}/>
-            <Route path="/buttons" element={<Buttons/>}/>
-          </Routes>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
           </div>
-          <div className="infos">
-            <h2 className="icons-link">Icons: <a href="https://material.io/resources/icons/?style=round" target="_blank">https://material.io/resources/icons/?style=round</a></h2>
-            <h2 className="created-by">created by <a href="https://devchallenges.io/portfolio/TheChiran" target="_blank">ChiranSWE</a> - devChallenges.io</h2>
-          </div>
+        </div>
+        <div className="footer-wrapper">
+          <Footer />
         </div>
       </div>
     </div>
