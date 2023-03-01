232259e3698981baa1db6a3414e85600c81cdfbe
TheChiran <tonmoychiran449@gmail.com>
2023-03-01 22:32:41 +0600

    [Task-> 1. integrated footer and footer design]

--- src/components/Footer/Index.tsx
+++ src/components/Footer/Index.tsx
@@ -1,5 +1,18 @@
-export default function Footer(){
+import logoWhite from "./../../assets/CatwikiLogo-white.svg";
+import "./footer.scss";
+
+export default function Footer() {
     return (
-        <h2>Footer</h2>
+        <footer>
+            <div className="logo">
+                <img src={logoWhite} alt="Cat Wiki logo white variant" />
+            </div>
+            <div className="author-wrapper">
+                <h2>
+                    <span className="copyright">&#169;</span>
+                    created by <a href="https://devchallenges.io/portfolio/TheChiran" target="_blank" className="author">ChiranSWE</a>-<a href="https://devchallenges.io/" className="url">devChallenge.io</a> <span className="date">{new Date().getFullYear()}</span>
+                </h2>
+            </div>
+        </footer>
     )
 }
