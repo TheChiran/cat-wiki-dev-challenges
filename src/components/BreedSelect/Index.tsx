582c4ead878c7d7b76e6f8c7952f905ebe8ff5fc
TheChiran <tonmoychiran449@gmail.com>
2023-03-06 22:37:51 +0600

    [Task-> 1. Integrated cat api 2. Added loader]

--- src/components/BreedSelect/Index.tsx
+++ src/components/BreedSelect/Index.tsx
@@ -1,26 +1,29 @@
 import "./select.scss";
+import CircularProgress from '@mui/material/CircularProgress';
 
 interface SelectItem {
     name: string;
 };
 
 type SelectProps = {
-    items: SelectItem[];
-    bindValue: any;
-    onSelect: () => {};
+    items: any[];
+    bindValue?: any;
+    onSelect?: () => {};
+    is_loading?: boolean;
 }
 
 const demoData = ['American Bobtail', 'American Curl', 'American Shorthair', 'American Wirehair', 'American Shorthair', 'American Wirehair'];
 
-export default function BreedSelect() {
+export default function BreedSelect({ items, is_loading }: SelectProps) {
     return (
         <div className="cat-bred-select">
+            {is_loading && <center><CircularProgress /></center>}
             <div className="cat-breed-select-list">
-                {
-                    demoData?.map((value) => {
+                {!is_loading &&
+                    items?.map((item: SelectItem) => {
                         return (
                             <div className="cat-bred-select__option">
-                                {value}
+                                {item.name}
                             </div>
                         )
                     })
