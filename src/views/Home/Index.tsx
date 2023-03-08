3f6ad121b1981da97da649b671289f806ec4cf3d
TheChiran <tonmoychiran449@gmail.com>
2023-03-08 22:39:17 +0600

    [Task-> 1. Implemented design for breeds]

--- src/views/Home/Index.tsx
+++ src/views/Home/Index.tsx
@@ -1,9 +1,11 @@
 import HomeBanner from "./components/Banner/Index";
+import Breeds from "./components/Breeds/Index";
 
 export default function Home() {
     return (
         <div className="home-section">
             <HomeBanner />
+            <Breeds />
         </div>
     )
 };
