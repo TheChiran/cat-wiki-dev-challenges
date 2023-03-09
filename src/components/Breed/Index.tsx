517619163524eebe3139feb39a32a478d92959ec
TheChiran <tonmoychiran449@gmail.com>
2023-03-09 22:02:44 +0600

    [Task-> 1. Implemented design for breed-detail]

--- src/components/Breed/Index.tsx
+++ src/components/Breed/Index.tsx
@@ -1,20 +1,20 @@
 import "./breed.scss";
 
 type BreedProps = {
-    name: string;
+    name?: string;
     imageUrl: string;
     hasSideOverlay?: boolean;
 };
 
 export default function Breed({
-    name, imageUrl, hasSideOverlay = false
+    name = '', imageUrl, hasSideOverlay = false
 }: BreedProps) {
     return (
         <div className={`breed`}>
             <div className={`image-wrapper ${hasSideOverlay ? 'side-overlay' : ''}`}>
                 <img src={imageUrl} alt="Cat Breed Image" />
             </div>
-            <h2 className="name">{name}</h2>
+            {name !== '' && <h2 className="name">{name}</h2>}
         </div>
     )
 };
