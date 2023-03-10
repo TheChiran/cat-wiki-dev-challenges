50ca356ee5fa185cbc1dd10038ee5f73254f4f6f
TheChiran <tonmoychiran449@gmail.com>
2023-03-10 23:38:08 +0600

    [Task-> 1. Implemented skeleton design for breed detail section]

---
+++ src/components/Skeletons/BreedDetail/Index.tsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+
+export default function BreedDetailSkeleton() {
+    return (
+        <div className="breed-detail-skeleton">
+            <div className="breed-detail-skeleton__image"></div>
+            <div className="breed-detail-skeleton__contents"></div>
+        </div>
+    );
+}
